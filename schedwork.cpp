c20194fb5ac9a1d7233ec04d1f137b4196ecd819
Lucas Rendina <rendina@usc.edu>
2024-04-19 23:47:17 +0000

    passing all tests

--- schedwork.cpp
+++ schedwork.cpp
@@ -1,3 +1,48 @@
+// #ifndef RECCHECK
+// #include <set>
+// #include <iostream>
+// #include <fstream>
+// #include <string>
+// #include <vector>
+// #include <map>
+// #include <algorithm>
+// // add or remove necessary headers as you please
+
+// #endif
+
+// #include "schedwork.h"
+
+// using namespace std;
+
+// // a constant that can be used to indicate an INVALID 
+// // worker ID if that is useful to your implementation.
+// // Feel free to not use or delete.
+// static const Worker_T INVALID_ID = (unsigned int)-1;
+
+
+// // Add prototypes for any helper functions here
+
+
+// // Add your implementation of schedule() and other helper functions here
+
+// bool schedule(
+//     const AvailabilityMatrix& avail,
+//     const size_t dailyNeed,
+//     const size_t maxShifts,
+//     DailySchedule& sched
+// )
+// {
+//     if(avail.size() == 0U){
+//         return false;
+//     }
+//     sched.clear();
+//     // Add your code below
+
+
+
+
+// }
+
 #ifndef RECCHECK
 #include <set>
 #include <iostream>
@@ -21,7 +66,13 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(const AvailabilityMatrix& avail,
+    size_t dailyNeed,
+    size_t maxShifts,
+    DailySchedule& sched,
+    size_t n,
+    size_t d,
+    vector<size_t>& numshifts);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -39,7 +90,52 @@ bool schedule(
     // Add your code below
 
 
+    if(avail[0].size()<dailyNeed){
+        return false;
+    }
 
+    vector<size_t> numshifts(avail[0].size(), 0);
 
+    for (size_t i = 0; i < avail.size(); i++) {
+        sched.push_back(vector<Worker_T>(dailyNeed, INVALID_ID));
+    }
+
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0, numshifts);
 }
 
+
+bool scheduleHelper(const AvailabilityMatrix& avail,
+     size_t dailyNeed,
+     size_t maxShifts,
+    DailySchedule& sched,
+     size_t n,
+     size_t d,
+    vector<size_t>& numshifts)
+{
+    if(d == dailyNeed && n==avail.size()-1){
+        return true;
+    }
+
+    if(d >= dailyNeed){
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, n+1, 0, numshifts);
+    }
+
+
+    for(size_t i=0;i<avail[0].size();i++){
+        sched[n][d] = i;
+        numshifts[i]++;
+
+        if(avail[n][i] ==1 && 
+        count(sched[n].begin(),sched[n].end(),i)==1 &&
+         numshifts[i] <= maxShifts){
+
+            if(scheduleHelper(avail,dailyNeed,maxShifts,sched,n,d+1,numshifts)){
+                return true;
+            } 
+
+        }
+        numshifts[i]--;
+    }
+    sched[n][d] = INVALID_ID;
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -1,3 +1,33 @@
+// #ifndef RECCHECK
+// // For debugging
+// #include <iostream>
+// // For std::remove
+// #include <algorithm> 
+// #include <map>
+// #include <set>
+// #endif
+
+// #include "wordle.h"
+// #include "dict-eng.h"
+// using namespace std;
+
+
+// // Add prototypes of helper functions here
+
+
+// // Definition of primary wordle function
+// std::set<std::string> wordle(
+//     const std::string& in,
+//     const std::string& floating,
+//     const std::set<std::string>& dict)
+// {
+//     // Add your code here
+
+// }
+
+// // Define any helper functions here
+
+
 #ifndef RECCHECK
 // For debugging
 #include <iostream>
@@ -14,6 +44,8 @@ using namespace std;
 
 // Add prototypes of helper functions here
 
+void helper(const string& in, const string& floating, const set<string>& dict, set<string>& words, size_t idx);
+
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +54,49 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    set<string> finwords;
+    if(in.size()!=0){
+        helper(in, floating, dict, finwords,0);
+    }
+    
+    return finwords;
 }
 
+
+
 // Define any helper functions here
+
+void helper(const string& in, const string& floating, const set<string>& dict, set<string>& finwords, size_t idx){
+
+    if( floating.size()==0){
+        while(in[idx] != '-'){
+            if(idx== in.size()){
+                break;
+            }
+            idx++;
+        }
+
+        if(idx== in.size()){
+            if(dict.find(in) != dict.end()){
+                finwords.insert(in);
+            }
+        }
+        else{
+            for(size_t i=0;i<26;i++){
+                string newin = in;
+                newin[idx] = i+ 'a';
+                helper(newin, floating, dict, finwords, idx+1);
+            }
+        }
+        return;
+    }
+
+    for(size_t i=0;i<in.size();i++){
+        string newfloating = floating.substr(0,floating.size()-1);
+        if(in[i]=='-'){
+            string newin = in;
+            newin[i] = floating[floating.size()-1];
+            helper(newin, newfloating, dict, finwords, 0);
+        }
+    }
+}
